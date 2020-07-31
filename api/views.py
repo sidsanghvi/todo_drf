c2449a35979d4ff298adabf3081aba40d6d1a2b1
Sid Sanghvi <sidsanghvi7@gmail.com>
2020-07-31 10:09:38 -0500

    add task GET logic

--- api/views.py
+++ api/views.py
@@ -4,6 +4,9 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .serializers import *
+from .models import *
+
 # Create your views here.
 
 
@@ -17,3 +20,17 @@ def apiOverview(request):
         'Delete': '/task-delete/<str:pk>/',
     }
     return Response(api_urls)
+
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
